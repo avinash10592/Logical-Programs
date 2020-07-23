3854b0913ed7c5eb4c92f70fc03017ba193a61bd
avinash10592 <59564861+avinash10592@users.noreply.github.com>
2020-07-23 23:10:04 +0530

    Add files via upload

---
+++ Logical Programs/Day2_logicalprograms/PositiveOrNegative.java
@@ -0,0 +1,14 @@
+package Day2_logicalprograms;
+
+public class PositiveOrNegative {
+    public static void main(String[] args) {
+        int number = 109;
+        if (number > 0) {
+            System.out.println(number + " is a positive number");
+        } else if (number < 0) {
+            System.out.println(number + " is a negative number");
+        } else {
+            System.out.println(number + " is neither positive nor negative");
+        }
+    }
+}
---
+++ Logical Programs/Day2_logicalprograms/SumOfNaturalNumbers.java
@@ -0,0 +1,15 @@
+package Day2_logicalprograms;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+
+        int num = 10, count = 1, total = 0;
+
+        while (count <= num) {
+            total = total + count;
+            count++;
+        }
+
+        System.out.println("Sum of first 10 natural numbers is: " + total);
+    }
+}
