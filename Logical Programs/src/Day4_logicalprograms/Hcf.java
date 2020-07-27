17323aa0604fa7ee19bcdaf2c22428a07b5848dc
avinash10592 <59564861+avinash10592@users.noreply.github.com>
2020-07-27 22:54:11 +0530

    Add files via upload

---
+++ Logical Programs/src/Day4_logicalprograms/Hcf.java
@@ -0,0 +1,15 @@
+package Day4_logicalprograms;
+
+public class Hcf {
+    public static void main(String arg[]) {
+        int n1 = Integer.parseInt(arg[0]);
+        int n2 = Integer.parseInt(arg[1]);
+        int temp;
+        while (n2 > 0) {
+            temp = n2;
+            n2 = n1 % n2;
+            n1 = temp;
+        }
+        System.out.println("hcf=" + n1);
+    }
+}
