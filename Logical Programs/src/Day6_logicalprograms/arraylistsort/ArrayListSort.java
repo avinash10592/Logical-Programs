26edcabae7521d03ebf76227b8b598b7f7b76dbc
avinash10592 <59564861+avinash10592@users.noreply.github.com>
2020-07-29 22:59:53 +0530

    Add files via upload

---
+++ Logical Programs/src/Day6_logicalprograms/arraylistsort/ArrayListSort.java
@@ -0,0 +1,21 @@
+package Day6_logicalprograms.arraylistsort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListSort {
+    public static void main(String args[]) {
+
+
+        ArrayList<Student> arraylist = new ArrayList<Student>();
+        arraylist.add(new Student(222, "Messi", 29));
+        arraylist.add(new Student(333, "Ronaldo", 31));
+        arraylist.add(new Student(111, "john", 23));
+
+        Collections.sort(arraylist);
+
+        for(Student str: arraylist){
+            System.out.println(str);
+        }
+    }
+}
