8e631508b4424f2b7f316805a15eab74208f8459
avinash10592 <59564861+avinash10592@users.noreply.github.com>
2020-07-30 22:13:20 +0530

    Add files via upload

---
+++ Logical Programs/src/Day7_logicalprograms/AddMethodExample.java
@@ -0,0 +1,29 @@
+package Day7_logicalprograms;
+
+import java.util.ArrayList;
+
+public class AddMethodExample {
+    public static void main(String args[]) {
+
+        // Declaration of String ArrayList
+        ArrayList<String> al = new ArrayList<String>();
+
+       /* Simple add() method for adding element
+          at the end of the ArrayList */
+        al.add("California");
+        al.add("Boston");
+        al.add("San jose");
+        al.add("New York");
+
+        //Adding element to the 3rd position
+        //3rd position = 2 index as index starts with 0
+        al.add(2, "San Francisco");
+        System.out.println("ArrayList after adding String San Francisco:" + al);
+
+        //Addition of String element at 1st position
+        al.add(0, "Texas");
+
+        //Displaying the ArrayList
+        System.out.println("ArrayList after adding String Texas:" + al);
+    }
+}
