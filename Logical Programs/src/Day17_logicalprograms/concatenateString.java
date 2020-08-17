1c4b9484d6f6317d3cef3431d33dbf00ff4e2f96
avinash10592 <59564861+avinash10592@users.noreply.github.com>
2020-08-17 21:42:58 +0530

    Add files via upload

---
+++ Logical Programs/src/Day17_logicalprograms/concatenateString.java
@@ -0,0 +1,14 @@
+package Day17_logicalprograms;
+
+public class concatenateString {
+    public static void main(String[] args) {
+        // First String as an Object
+        String str = "Hello";
+
+        // Concatenating and storing above String object and constant string
+        String result = str + "World";
+
+        // Print the result
+        System.out.println(result);
+    }
+}
