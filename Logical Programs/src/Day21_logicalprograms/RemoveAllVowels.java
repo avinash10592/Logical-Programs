c5a19d7fe158a7cfcbb7185c1f3d7f964f4925d9
avinash10592 <59564861+avinash10592@users.noreply.github.com>
2020-08-21 21:49:44 +0530

    Add files via upload

---
+++ Logical Programs/src/Day21_logicalprograms/RemoveAllVowels.java
@@ -0,0 +1,21 @@
+package Day21_logicalprograms;
+
+import java.util.Scanner;
+
+public class RemoveAllVowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the string...");
+
+        String inputString = sc.nextLine();
+
+        String newInputString = inputString.replaceAll("[AEIOUaeiou]", "");
+
+        System.out.println("The string without vowels...");
+
+        System.out.println(newInputString);
+
+        sc.close();
+    }
+}
