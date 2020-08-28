2ffa898be985afb8f924f9f7a7425971cd5a4a17
avinash10592 <59564861+avinash10592@users.noreply.github.com>
2020-08-28 19:02:13 +0530

    Add files via upload

---
+++ Logical Programs/src/Day26_logicalprograms/NumberOrNot.java
@@ -0,0 +1,30 @@
+package Day26_logicalprograms;
+
+import java.util.Scanner;
+
+public class NumberOrNot {
+    static boolean numberOrNot(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+        return true;
+    }
+}
+
+class CheckMobileNumber {
+    public static void main(String[] args) {
+        System.out.println("Enter your mobile number");
+
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+
+        if (NumberOrNot.numberOrNot(input) && (input.length() == 10)) {
+            System.out.println("Good!!! You have entered valid mobile number");
+        } else {
+            System.out.println("Sorry!!!! You have entered invalid mobile number. Try again...");
+        }
+    }
+}
