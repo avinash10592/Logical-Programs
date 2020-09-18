609f16edf8daaaf8281be9f429401206abe4b068
avinash10592 <59564861+avinash10592@users.noreply.github.com>
2020-09-18 18:05:33 +0530

    Add files via upload

---
+++ Logical Programs/src/Day36_logicalprograms/ArrayListLoopExample1.java
@@ -0,0 +1,51 @@
+package Day36_logicalprograms;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayListLoopExample1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        al.add(13);
+        al.add(7);
+        al.add(36);
+        al.add(89);
+        al.add(97);
+
+
+        /*For Loop Example for traversing ArrayList*/
+        System.out.println("Using For Loop");
+        for (int counter = 0; counter < al.size(); counter++) {
+            System.out.println(al.get(counter));
+        }
+
+        /* Advanced For Loop Example for iterating ArrayList*/
+        System.out.println("Using Advanced For Loop");
+        for (Integer num : al) {
+            System.out.println(num);
+        }
+
+        /* While Loop Example for iterating ArrayList*/
+        System.out.println("Using While Loop");
+        int count = 0;
+        while (al.size() > count) {
+            System.out.println(al.get(count));
+            count++;
+        }
+
+        /* Iterator Example for Traversing ArrayList */
+        System.out.println("Using Iterator");
+        Iterator it = al.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        /* Collection stream() util example */
+        System.out.println("Using Collection stream() util");
+        al.forEach((num) -> {
+            System.out.println(num);
+        });
+
+    }
+
+}
---
+++ Logical Programs/src/Day36_logicalprograms/concatenateString1.java
@@ -0,0 +1,14 @@
+package Day36_logicalprograms;
+
+public class concatenateString1 {
+    public static void main(String[] args) {
+        // First String as an Object
+        String str = "Hello";
+
+        // Concatenating and storing above String object and constant string
+        String result = str + "World";
+
+        // Print the result
+        System.out.println(result);
+    }
+}
