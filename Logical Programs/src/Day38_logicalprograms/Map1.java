97119ac0c77dc511a137594afa4fec9d5b38cd4d
avinash10592 <59564861+avinash10592@users.noreply.github.com>
2020-10-12 18:26:48 +0530

    Add files via upload

---
+++ Logical Programs/src/Day38_logicalprograms/Map1.java
@@ -0,0 +1,17 @@
+package Day38_logicalprograms;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Map1 {
+    public static void main(String[] str) {
+        List<String> Names = Arrays.asList("Avinash", "Riyaz", "Rohit");
+
+        List<String> UpperCase = Names.stream().map(String::toUpperCase).collect(Collectors.toList());
+        // Changed the characters into upper case after converting it into Stream
+
+        UpperCase.forEach(System.out::println);
+        // Printed using forEach Terminal Operation
+    }
+}
