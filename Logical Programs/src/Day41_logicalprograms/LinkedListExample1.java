92e5ef79721eddaa5cca6c72a719d92373a806b4
avinash10592 <59564861+avinash10592@users.noreply.github.com>
2020-10-16 19:09:32 +0530

    Add files via upload

---
+++ Logical Programs/src/Day41_logicalprograms/LinkedListExample1.java
@@ -0,0 +1,49 @@
+package Day41_logicalprograms;
+
+import java.util.LinkedList;
+
+public class LinkedListExample1 {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+
+        //Adding elements at the end of the list
+
+        list.add("JAVA");
+
+        list.add("J2EE");
+
+        list.add("JSP");
+
+        list.add("SERVLETS");
+
+        list.add("JDBC");
+
+        //Printing the elements of list
+
+        System.out.println(list);
+
+        String s = "JSP";
+
+        //Checking whether list contains "JSP"
+
+        boolean contains = list.contains(s);
+
+        if (contains) {
+            //If list contains "JSP", printing it's index
+
+            System.out.println(list.indexOf(s));      //Output : 2
+        }
+
+        s = "STRUTS";
+
+        //Checking whether list contains "STRUTS"
+
+        contains = list.contains("STRUTS");
+
+        if (contains) {
+            //If list contains "STRUTS", printing it's index
+
+            System.out.println(list.indexOf(s));
+        }
+    }
+}
---
+++ Logical Programs/src/Day41_logicalprograms/ReverseEachWord1.java
@@ -0,0 +1,34 @@
+package Day41_logicalprograms;
+
+public class ReverseEachWord1 {
+    static void reverseEachWordOfString(String inputString) {
+        String[] words = inputString.split(" ");
+
+        String reverseString = "";
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+
+            String reverseWord = "";
+
+            for (int j = word.length() - 1; j >= 0; j--) {
+                reverseWord = reverseWord + word.charAt(j);
+            }
+
+            reverseString = reverseString + reverseWord + " ";
+        }
+
+        System.out.println(inputString);
+
+        System.out.println(reverseString);
+
+        System.out.println("-------------------------");
+    }
+
+    public static void main(String[] args) {
+        reverseEachWordOfString("Hello my name is Avinash");
+
+        reverseEachWordOfString("How are you today");
+
+    }
+}
